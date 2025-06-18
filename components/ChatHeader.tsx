c993427be2c49114bb15afdae35c71bd57f28e81
ibraSanusi <isanu227@gmail.com>
2025-06-18 11:14:25 +0200

    style

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -1,7 +1,7 @@
 import OpenAI from "@/components/icons/open-ai";
 import { HiOutlineDocumentAdd } from "react-icons/hi";
 import { GoSidebarCollapse } from "react-icons/go";
-import { Button } from "./ui/button";
+import { Button } from "./ui/my-button";
 
 export const ChatHeader = () => (
   <header className="sticky top-5 right-5 left-5 z-50 mt-4 flex flex-row items-center justify-between rounded-md border bg-white/70 px-4 py-2 backdrop-blur-lg">
