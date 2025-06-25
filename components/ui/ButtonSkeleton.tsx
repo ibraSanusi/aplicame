6b4c109f25a8feb13b79f071b180e4e922e854ed
ibraSanusi <isanu227@gmail.com>
2025-06-25 18:15:01 +0200

    sytle(home-page): added button and table skeleton

---
+++ components/ui/ButtonSkeleton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./button";
+
+export default function ButtonSkeleton() {
+  return (
+    <Button disabled className="animate-pulse bg-gray-300">
+      Click
+    </Button>
+  );
+}
