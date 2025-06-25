f18c8850daba249ebdb986d693adbb7f45675393
ibraSanusi <isanu227@gmail.com>
2025-06-25 17:35:24 +0200

    fixed(fetchApplications): doesn't fetch the applications if user not in session

--- hooks/useApplications.ts
+++ hooks/useApplications.ts
@@ -1,20 +1,34 @@
 import { useApplicationStore } from "@/store/store";
 import { type Application } from "@prisma/client";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 export function useApplication() {
-  const { applications, addApplication } = useApplicationStore();
+  const { applications, addApplication, clearApplications } =
+    useApplicationStore();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    async function fetchApplications() {
-      try {
-        const res = await fetch("/api/application");
-        if (!res.ok) throw new Error("Error al cargar las solicitudes");
-        const data: Application[] = await res.json();
+  const session = useSession();
 
-        data.forEach(addApplication); // Guardamos todo en el store
+  useEffect(() => {
+    if (session.status !== "authenticated") {
+      clearApplications();
+      setLoading(false);
+      return;
+    }
+
+    async function fetchApplications() {
+      setLoading(true);
+      setError(null);
+      try {
+        const applicationResponse = await fetch("/api/application");
+        if (!applicationResponse.ok)
+          throw new Error("Error al cargar las solicitudes");
+        const data: Application[] = await applicationResponse.json();
+
+        clearApplications();
+        data.forEach(addApplication);
       } catch (err) {
         if (err instanceof Error) {
           setError(err.message);
@@ -25,8 +39,9 @@ export function useApplication() {
         setLoading(false);
       }
     }
+
     fetchApplications();
-  }, [addApplication]);
+  }, [session.status, addApplication, clearApplications]);
 
   return { applications, loading, error };
 }
