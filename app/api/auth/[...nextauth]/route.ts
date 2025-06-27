9429de65d21a48878ea9c08141b13c1513cfbc20
ibraSanusi <isanu227@gmail.com>
2025-06-27 19:32:01 +0200

    fix: now nextauth route only export post and get so they will be no conflicts when buillding the project

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { db } from "@/lib/db";
-import { env } from "@/env.mjs";
+import { authOptions } from "@/lib/authOptions";
 
 // Extiende los tipos de NextAuth para incluir accessToken en Session y JWT
 declare module "next-auth" {
@@ -16,49 +12,6 @@ declare module "next-auth/jwt" {
     accessToken?: string;
   }
 }
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope:
-            "openid email profile https://www.googleapis.com/auth/calendar.events",
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        console.log("Google account from JWT callback:", account);
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token && typeof token.accessToken === "string") {
-        session.accessToken = token.accessToken;
-      } else {
-        console.warn("Token does not contain accessToken:", token);
-      }
-      return session;
-    },
-    async redirect({ url, baseUrl }) {
-      console.log("Redirect URL:", url);
-      console.log("Base URL:", baseUrl);
-      // Siempre redirige a la home tras login
-      return baseUrl;
-    },
-  },
-  adapter: PrismaAdapter(db),
-  session: {
-    strategy: "jwt",
-  },
-  secret: env.NEXTAUTH_SECRET,
-};
 
 const handler = NextAuth(authOptions);
 
