c659f7f88dd0b28b4c2d7053cab2f9a964e636ba
ibraSanusi <isanu227@gmail.com>
2025-07-05 20:04:20 +0200

    remove collapse button

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -6,6 +6,7 @@ import { ChatHeader } from "./ChatHeader";
 import { Messages } from "./Messages";
 import { useChat } from "@/hooks/useChat";
 import { Role } from "@/lib";
+import { cn } from "@/lib/utils";
 
 export const initialMessages = [
   {
@@ -19,7 +20,11 @@ export default function Chat() {
     useChat(initialMessages);
 
   return (
-    <aside className="chat-scrollbar relative col-span-1 mx-auto flex h-dvh w-full max-w-xl flex-col overflow-y-scroll border-r border-gray-200 bg-[#E8F1F2] px-6">
+    <aside
+      className={cn(
+        "chat-scrollbar relative col-span-1 mx-auto flex h-dvh w-full max-w-xl flex-col overflow-y-scroll border-r border-gray-200 bg-[#E8F1F2] px-6",
+      )}
+    >
       <ChatHeader resetMessages={resetMessages} />
       <Messages loadingMessage={loadingMessage} messages={messages} />
       <MessageInput onSend={handleSend} />
