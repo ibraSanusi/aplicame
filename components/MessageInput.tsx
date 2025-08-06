03295d75f3bacdda610eff15344d6d87aefd5c61
Ibrahim Sanusi <ibrahim@fullcircle.es>
2025-08-06 18:49:32 +0200

    refactor: reorganized logic

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 // components/MessageInput.tsx
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent } from "react";
 import { AiOutlineSend } from "react-icons/ai";
+import { TextArea } from "./textarea";
+import { useTextarea } from "@/hooks/useTextarea";
 
 interface MessageInputProps {
   onSend: (text: string) => void;
 }
 
 export default function MessageInput({ onSend }: MessageInputProps) {
-  const [text, setText] = useState("");
-  const formRef = useRef<HTMLFormElement>(null);
+  const { formRef, setText, text } = useTextarea();
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -23,24 +24,18 @@ export default function MessageInput({ onSend }: MessageInputProps) {
   return (
     <div className="sticky right-5 bottom-5 left-5 z-50 mt-4 flex max-w-[425px] gap-2">
       <form ref={formRef} onSubmit={handleSubmit} className="relative w-full">
-        <textarea
+        <TextArea
           name="message"
-          onKeyDown={(event) => {
-            if (event.key === "Enter" && !event.shiftKey) {
-              event.preventDefault();
-              formRef.current?.requestSubmit();
-            }
-          }}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
           placeholder="Escribe tu mensaje..."
-          className="border-border placeholder:text-placeholder flex field-sizing-content max-h-[175px] min-h-20 w-full flex-grow items-start rounded-md border bg-white/70 p-3 backdrop-blur-lg outline-none placeholder:text-sm"
+          onChange={(e) => setText(e.target.value)}
+          text={text}
+          formRef={formRef}
         />
 
         <button
           type="submit"
           className="bg-dark absolute right-2 bottom-2 flex h-8 w-8 items-center justify-center rounded-full bg-[#1B98E0] p-2 transition-colors hover:bg-[#157ab7]"
-        // disabled={isLoading}
+          // disabled={isLoading}
         >
           <AiOutlineSend color="#ffffff" width={12} height={14} />
         </button>
